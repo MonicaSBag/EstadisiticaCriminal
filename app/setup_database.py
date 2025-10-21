070992631170c352ce41e2f529a0b853668bb73c
MonicaSBag <monicasosa97@gmail.com>
2025-10-21 15:18:44 -0300

    creacion db y docker para metabase

---
+++ app/setup_database.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from peewee import *
+#MODELO ORM
+#se crea la base de datos o lee si ya existe
+sqlite_db = SqliteDatabase('../database/estadistica_criminal.db')
+
+# ESTRUCTURA DE LA BASE DE DATOS
+class BaseModel(Model):
+    class Meta:
+        database = sqlite_db
+        table_name = 'estadisticas_delitos'
+    id = AutoField()
+    provincia_id = IntegerField()
+    provincia_nombre = CharField(max_length=50)
+    anio = IntegerField()
+    codigo_delito_snic_id = IntegerField()
+    codigo_delito_snic_nombre = CharField(max_length=100)
+    cantidad_hechos = IntegerField()
+    cantidad_victimas = IntegerField()
+    cantidad_victimas_masc = IntegerField()
+    cantidad_victimas_fem = IntegerField()
+    cantidad_victimas_sd = IntegerField()
+    tasa_hechos = FloatField()
+    tasa_victimas = FloatField()
+    tasa_victimas_masc = FloatField()
+    tasa_victimas_fem = FloatField()
+
+# Crear tabla si no existe
+sqlite_db.connect()
+sqlite_db.create_tables([BaseModel])
+print("Tabla creada o ya existente.")
+
+# Se lee el archivo xlsx y se carga los datos
+df = pd.read_excel("../snic-provincias.xlsx")
+with sqlite_db.atomic():
+    for _, row in df.iterrows():
+        BaseModel.create(**row.to_dict())
+
+print(f"{len(df)} registros cargados desde 'snic-provincias.xlsx'.")
